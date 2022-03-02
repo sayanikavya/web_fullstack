30065bd0efc63133de3a58d18dd4369ec687d3a3
sayanikavya <kssayani@gmail.com>
2022-03-02 14:49:45 -0600

    modified files

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+alert(fruit);
+fruit='orange';
+alert('i had '+ fruit +' for breakfast');
+('i had "apple" fruit for breakfast;');
+//var Name='kavya';
+var Name;
+console.log();
+console.log(10+10+'2');
